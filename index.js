9f96143fe19e7afd6d52bfbe0946c98d55f7e3b2
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-15 09:54:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+let routine =[];
+
+function wakeDog(dogName, dogBreed) {
+  routine.push(`wakeDog`);
+    return `Wake ${dogName} the ${dogBreed}`;
+}
+
+
+
+function leashDog(dogName, dogBreed) {
+  routine.push(`leashDog`);
+    return `Leash ${dogName} the ${dogBreed}`;
+}
+
+
+
+function walkToPark(dogName, dogBreed) {
+  routine.push(`walkToPark`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`;
+}
+
+
+
+
+function throwFrisbee(dogName, dogBreed) {
+  routine.push(`throwFrisbee`);
+   return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+}
+
+
+
+
+function walkHome(dogName, dogBreed) {
+  routine.push(`walkHome`);
+    return `Walk home with ${dogName} the ${dogBreed}`;
+}
+
+
+
+
+function unleashDog(dogName, dogBreed) {
+  routine.push(`unleashDog`);
+    return `Unleash ${dogName} the ${dogBreed}`;
+}
+
+
+
